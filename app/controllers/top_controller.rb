bf9b28a7223c3186caf440616d19097183f07b81
Hironori Akaishi <ah.gm3622@gmail.com>
2017-09-17 00:05:20 +0900

    change-top

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,11 @@
 class TopController < ApplicationController
   def index
   end
+
+  def feach_gmail
+    format.html{ render :nothing => true }
+    gmail = Gmail.new("h.akaishi.dev@gmail.com", "19940927")
+
+    puts gmail.inbox.count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'top#index'
 
   get 'top/index', to: 'top#index'
+  get 'top/fetch_gmail', to: 'top#fetch_gmail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
