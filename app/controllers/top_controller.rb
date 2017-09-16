6b3f5a16deafbc0adfea4fdb8e890f8807c1a63b
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2017-09-17 01:05:21 +0900

    write methods

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -6,14 +6,26 @@ class TopController < ApplicationController
     #Ringからのリクエストを受けて、最新のメールを取得する
     #session[:next_send_mail]に保存する
     #session[:sended_for_unity]=falseにする
+    
+    session[:next_send_mail] = fetch_gmail
+    session[:sended_for_unity] = false
   end
   
   def send_mail_for_unity
     #session[:next_send_mail]を取得してRender
     #session[:sended_for_unity]=trueにする
-    #    json = {
-    #      :text => session[:latest_email]
-    #    }
+    
+    if session[:sended_for_unity]
+      #送ってあるなら何も返さない
+      render :nothing
+    else
+      #送ってないなら送ってsession[:sended_for_unity] = true
+      json = {
+        :text => session[:next_send_email]
+      }
+      session[:seneded_for_unity] = true
+      render :json => json
+    end
   end
 
   def send_email_for_unity
@@ -26,7 +38,14 @@ class TopController < ApplicationController
   def new_mail_arrive?
     #最新のメールが既読かどうかを判定してunityに送る
     #session[:next_send_mail]と、最新email情報が一致するかどうか調べる
-
+    
+    new_mail = fetch_gmail
+    if new_mail != session[:next_send_mail]
+      #最新のメールはRingからのリクエストでとったものと一致しない時
+      render :text => "true"
+    else
+      render :text => "false"
+    end
   end
 
   def feach_gmail
