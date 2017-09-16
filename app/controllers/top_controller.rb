9e884c942bc7ba206712063e1588e2cd8ef7bbb7
Hironori Akaishi <ah.gm3622@gmail.com>
2017-09-17 01:19:26 +0900

    fetch-gmail

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,12 +1,14 @@
 class TopController < ApplicationController
   def index
   end
-  def feach_gmail
-    format.html{ render :nothing => true }
-    gmail = Gmail.new("h.akaishi.dev@gmail.com", "19940927")
 
-    puts gmail.inbox.count
+  def fetch_gmail
+    gmail = Gmail.new("h.akaishi.dev@gmail.com", "19940927")
+    get_mail = gmail.inbox.emails(:all).last
+    render 'index'
+    return get_mail
   end
+
   def send_email_for_unity
     json = {
       :text => "test"
