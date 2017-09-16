8cd12f65359d7d70238e774cef4468862f9305a9
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2017-09-16 23:29:32 +0900

    send_email test

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'top#index'
 
   get 'top/index', to: 'top#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'top/send_email_for_unity', to: 'top#send_email_for_unity'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
